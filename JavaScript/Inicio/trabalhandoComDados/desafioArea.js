63e30d9a6fa6dae06dfcc23b1e1736da9d733d28
guilherme <cezarguilherme03@gmail.com>
2023-11-18 17:55:09 -0300

    exercicios sábado

--- JavaScript/Inicio/trabalhandoComDados/desafioArea.js
+++ JavaScript/Inicio/trabalhandoComDados/desafioArea.js
@@ -18,10 +18,33 @@
 // ******************** ATIVIDADE 01 ******************************
 
 //Faça um programa que calcule a circurferência (área) de um círculo,
-//utilizando conceitos que já vimos anteriormente, tais como,
+//utilizando alguns conceitos, tais como,
 //variáveis, operadores aritméticos, concatenação, etc. 
 // lembre-se da estrutura dos dados (Entrada de dados, Processamento e Saída de dados)
 // tendo em mente que o raio da circunferência é de R = 16
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 //entrada de dados
 
--- JavaScript/Inicio/trabalhandoComDados/script01.js
+++ JavaScript/Inicio/trabalhandoComDados/script01.js
@@ -1,15 +1,40 @@
 //    camelCase
-var fabricanteCarro = "renault" 
-var modeloCarro = 'logan'
-var precoCarro = 16000
-var anoCarro = 2018
+// var fabricanteCarro = "renault" 
+// var modeloCarro = 'logan'
+// var precoCarro = 16000
+// var anoCarro = 2018
 
 // fabricante: renault, modelo: logan, preco: 16000, ano: 2018
 
 // console.log("fabricante: " + fabricanteCarro + ", modelo: " + modeloCarro + ", preco: " + precoCarro + ", ano: " + anoCarro)
 
-modeloCarro = "sandero"
-console.log(modeloCarro)
+// modeloCarro = "sandero"
+// console.log(modeloCarro)
 
-modeloCarro = "clio"
-console.log(modeloCarro)
+// modeloCarro = "clio"
+// console.log(modeloCarro)
+
+
+
+
+
+
+
+
+
+// trabalhar com variáveis, console.log
+
+//utilizando padrão camelCase
+
+//variavel(let, var, const) -> identificador -> dados
+const carro = "4x4"
+const fabricanteCarro = "renault"
+const modeloCarro = "sandero"
+const corCarro = "vermelha"
+const anoCarro = 2010
+
+//Primeira forma: Concatenação
+console.log("carro: " + carro + ", fabricante: " + fabricanteCarro + ", modelo do carro: " + modeloCarro + ", cor do carro: " + corCarro + ", ano: " + anoCarro);
+
+//Segunda forma: Interpolação
+console.log(`meu carro: ${carro}, fabricante do carro: ${fabricanteCarro}, modelo carro: ${modeloCarro}, cor do carro: ${corCarro}, ano do carro: ${anoCarro}`);
