f8ad16e98ced47331454678893d8c36c834d783d
Guilherme Amorim <cezarguilherme03@gmail.com>
2024-01-27 09:26:34 -0300

    Realizando consumo api

--- JavaScript/Inicio/trabalhandoComDOM/Sabado/ViaCEPProject/js/script.js
+++ JavaScript/Inicio/trabalhandoComDOM/Sabado/ViaCEPProject/js/script.js
@@ -1,4 +1,4 @@
-//dunção pesquisarCep ainda não foi criada
+//função pesquisarCep ainda não foi criada
 //focusout -> quando sair do focus do input
 
 //criando a função para limpar form
@@ -19,11 +19,11 @@ const preencherForm = (endereco) => {
 
 //avaliando se o CEP contem apenas números
 function eNumero(numero) {
-    return  /^[0-9]+$/.test(numero); //regularExpressions
+    return /^[0-9]+$/.test(numero); //regularExpressions
 }
 
 //avaliando de o CEP tem o comprimento de 8 caracteres e se também contém apenas números
-const cepValido = (cep) => cep.length == 8 && eNumero(cep); 
+const cepValido = (cep) => cep.length == 8 && eNumero(cep);
 
 //inicio da função para se conectar com a API do ViaCEP
 //alterar para função
@@ -36,21 +36,24 @@ const pesquisarCep = async () => {
     const url = `https://viacep.com.br/ws/${cep}/json/`
 
     if (cepValido(cep)) {
-        
-    //precisamos ter o retorno do viaCEP (JSON ) que ele vai retornar pra gente, através do fetch
-    //ele vai retornar uma promessa (algo que pode acontecer ou não), por isso ele é assincrono
-    //ele vai mandar o dado do retorno no console log
-    //fetch(url).then(response => response.json()); //utilizamos o json para retornar a resposta da api(viacep)
 
-    const dados = await fetch(url) //await => aguardar os dados
-    const endereco = await dados.json() //vamos utilizar os dados que estão sendo guardados no dados e exibir na tela
+        //precisamos ter o retorno do viaCEP (JSON ) que ele vai retornar pra gente, através do fetch
+        //ele vai retornar uma promessa (algo que pode acontecer ou não), por isso ele é assincrono
+        //ele vai mandar o dado do retorno no console log
+        //fetch(url).then(response => response.json()); //utilizamos o json para retornar a resposta da api(viacep)
 
-    endereco.hasOwnProperty('erro') ?  document.getElementById('endereco').value = 'CEP não encontrado!' : preencherForm(endereco);
+        const dados = await fetch(url) //await => aguardar os dados
+        const endereco = await dados.json() //vamos utilizar os dados que estão sendo guardados no dados e exibir na tela
 
-    }else{
+        console.log(endereco);
+
+        // retorna um booleano indicando se o objeto possui a propriedade especificada
+        endereco.hasOwnProperty('erro') ? document.getElementById('endereco').value = 'CEP não encontrado!' : preencherForm(endereco);
+
+    } else {
         document.getElementById('endereco').value = 'CEP incorreto!'
-    }   
-    
+    }
+
 }
 
 document.getElementById('cep')
